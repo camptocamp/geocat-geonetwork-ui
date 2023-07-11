0b91596d5ab49d82a795381b904a0e1c8f9713a6
Florent gravin <florent.gravin@camptocamp.com>
2023-07-11 15:08:01 +0200

    feat: add TW safelist classes

--- apps/datahub/tailwind.config.js
+++ apps/datahub/tailwind.config.js
@@ -4,7 +4,7 @@ const { join } = require('path')
 
 module.exports = {
   ...baseConfig,
-  safelist: [...baseConfig.safelist, 'w-max'],
+  safelist: [...baseConfig.safelist, 'w-max', 'items-end'],
   theme: {
     ...baseConfig.theme,
     extend: {
