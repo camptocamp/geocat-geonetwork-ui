d88c1264bccaf21227652d71f50828dfec8a194b
Florent Gravin <florent.gravin@camptocamp.com>
2024-06-11 16:40:14 +0200

    search: do not exclude services or maps

--- libs/api/repository/src/lib/gn4/elasticsearch/elasticsearch.service.ts
+++ libs/api/repository/src/lib/gn4/elasticsearch/elasticsearch.service.ts
@@ -304,7 +304,6 @@ export class ElasticsearchService {
     return {
       bool: {
         must,
-        must_not,
         should,
         filter,
       },
@@ -353,14 +352,6 @@ export class ElasticsearchService {
               },
             },
           ],
-          must_not: {
-            ...this.queryFilterOnValues('resourceType', [
-              'service',
-              'map',
-              'map/static',
-              'mapDigital',
-            ]),
-          },
         },
       },
       _source: ['resourceTitleObject', 'uuid'],
